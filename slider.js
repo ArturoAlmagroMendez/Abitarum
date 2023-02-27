9da758e5d5810240cbea561316dbeaaa1c40c4b4
DESKTOP-6NU7L00\almag <almagromendezarturo@gmail.com>
2023-02-27 02:16:28 +0100

    created the Showroom Section and changed the nav style

---
+++ slider.js
@@ -0,0 +1,11 @@
+let currentSlide = 0;
+
+function startSlider() {
+    let slider = document.getElementById("slider");
+    let sliderImagesCount = slider.querySelectorAll("img");
+    
+}
+setInterval(() => {
+    startSlider();    
+}, 5000);
+
