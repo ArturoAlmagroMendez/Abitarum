5b274062c0851ad849eb4152b48deeb2c4ded0cb
ArturoAlmagroMendez <almagromendezarturo@gmail.com>
2023-02-27 12:45:25 +0100

    new slider style

--- slider.js
+++ slider.js
@@ -1,11 +1,62 @@
-let currentSlide = 0;
+window.addEventListener("load", () => {
+  // initial slide
+  let slide = 1;
 
-function startSlider() {
-    let slider = document.getElementById("slider");
-    let sliderImagesCount = slider.querySelectorAll("img");
-    
-}
-setInterval(() => {
-    startSlider();    
-}, 5000);
+  // total slides
+  let slides = document.querySelectorAll(".slider ul li");
+  total = slides.length;
 
+  // show first side
+  showSlide(1);
+
+  next = document.querySelector(".next");
+  prev = document.querySelector(".prev");
+  
+  /**
+   * event next button
+   */
+  function nextImage() {
+    slide++;
+    if (slide > total) {
+      slide = 1;
+    }
+    showSlide(slide);
+  }
+
+  next.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    nextImage();
+  });
+  /*Event prev button */
+  function prevImage() {
+    slide--;
+    if (slide < 1) {
+      slide = total;
+    }
+    showSlide(slide);
+  }
+
+  prev.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    prevImage();
+  });
+
+  /**
+   * show slides
+   *
+   * @param {number} n
+   * @return {null}
+   *
+   */
+  function showSlide(n) {
+    n--; // decrement 1
+    for (i = 0; i < slides.length; i++) {
+      i == n
+        ? (slides[n].style.display = "block")
+        : (slides[i].style.display = "none");
+    }
+  }
+  const interval = setInterval(function () {
+    nextImage();
+  }, 5000);
+});
